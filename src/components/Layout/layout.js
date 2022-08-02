e8e2f75982e57fa94c32782fcc2fb007491986d1
apozzi <axelpozzi@gmail.com>
2022-08-01 23:09:49 -0500

    working on nav

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Layout =({ pageTitle,children }) => {
     return(
         <div>
             <title>{pageTitle}</title>
-            <nav className='flex justify-end list'>
-                    <li className='pa1'><Link to="/">Home</Link></li>
-                    <li className='pa1'><Link to="/projects">Projects</Link></li>
-            </nav>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="/">AP</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/projects">Projects</Nav.Link>
+                    <Nav.Link href="/resume">Resume</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
             <main>
                 {children}
             </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from '../components/Layout/layout';
-import 'tachyons';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const IndexPage = () => {
   return (
