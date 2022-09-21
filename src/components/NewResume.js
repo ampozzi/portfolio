44c97e13c2dade7adac8d7572ae6230a267c9504
Axel Pozzi <axelpozzi@gmail.com>
2022-09-20 20:00:41 -0500

    Fixed resume features, added download

--- src/components/NewResume.js
+++ src/components/NewResume.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
-import { Paper, Container, Grid, Typography,Box } from '@mui/material';
-import Resume from '../assets/Profile.pdf'
+import { Grid, Box, Button, Container} from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
+import Resume from '../assets/Profile.pdf';
 
 export default function Test() {
   const [numPages, setNumPages] = useState(null);
@@ -11,22 +12,41 @@ export default function Test() {
   }
 
   return (
-    <Container>
-      <Document
-        file={Resume}
-        onLoadSuccess={onDocumentLoadSuccess}
+    <Container 
+    maxWidth='lg'
+    sx={{height:'100%'}}>
+      <Grid container
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      sx={{height:'100%'}}
       >
-        {Array.from(
-          new Array(numPages),
-          (el, index) => (
-            <Page
-              key={`page_${index + 1}`}
-              pageNumber={index + 1}
-              width='900'
-            />
-          ),
-        )}
-      </Document>
+        <Box my={1}>
+          <Button 
+          variant="contained" 
+          startIcon={<DownloadIcon />}
+          href={Resume}
+          >
+            Download</Button>
+          </Box>
+        <Grid item>
+          <Document
+            file={Resume}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            {Array.from(
+              new Array(numPages),
+              (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  width='1000'
+                />
+              ),
+            )}
+          </Document>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
